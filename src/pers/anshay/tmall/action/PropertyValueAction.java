8c441e4c8ff7b0758b50cbde7a247e56cefb09b5
Anshay <905928827@qq.com>
2018-07-27 12:05:00 +0800

    优化

--- src/pers/anshay/tmall/action/PropertyValueAction.java
+++ src/pers/anshay/tmall/action/PropertyValueAction.java
@@ -2,8 +2,6 @@ package pers.anshay.tmall.action;
 
 import org.apache.struts2.convention.annotation.Action;
 
-import pers.anshay.tmall.service.PropertyValueService;
-
 /**
  * @author Anshay
  * @date 2018年7月13日
