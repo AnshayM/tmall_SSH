c5d5d78cfb595e0c993875fd99f3eb100e9e277d
Anshay <905928827@qq.com>
2018-07-27 09:35:15 +0800

    修改注释

--- src/pers/anshay/tmall/service/OrderItemService.java
+++ src/pers/anshay/tmall/service/OrderItemService.java
@@ -10,11 +10,14 @@ import pers.anshay.tmall.pojo.Order;
  * @explain
  */
 public interface OrderItemService extends BaseService {
-	/* 递归调用为为多个订单计算 */
+	/**
+	 * 递归调用为为多个订单计算
+	 */
 	public void fill(List<Order> orders);
 
-	/*
+	/**
 	 * 为订单对象填充其orderItems字段，并且计算出订单总金额，订单总购买数量
 	 */
 	public void fill(Order order);
+
 }
